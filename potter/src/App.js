1067206626bf3f54b0bf8a216a3d5d340a8dacf0
Dahesey <dahesey@gmail.com>
2024-09-25 18:29:54 +0000

    Getting started with styling the page

---
+++ potter/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>
+            This is the body of the page. 
+          </h1>
+        </div>
+      )
+    }
+  }
+
+  export default App
